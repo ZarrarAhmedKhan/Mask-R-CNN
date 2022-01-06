0b54cf112b75d655c94e69511d3643fdf009d430
ZarrarAhmedKhan <zarrar.niazi3760@gmail.com>
2022-01-06 15:31:29 +0500

    update

--- config.py
+++ config.py
@@ -1,16 +1,35 @@
 from mrcnn.config import Config
 
-ROOT_DIR = "root directory of this repo"
-pretrained_model = "mask_rcnn_coco.h5"
+ROOT_DIR = os.path.abspath(os.getcwd()) # Root directory of the project
+pretrained_model = "mask_rcnn_coco.h5" # Path to trained weights file
+
 checkpoints_dir = "logs"
-dataset_folder = "dataset" #dataset folder path
 train_subset = 'train'
+
 val_subset = 'val'
 json_file = "via_project.json" # same name of json_file in both train and val
 
-class CustomConfig(Config):
+############################################################
+#  Configurations
+############################################################
+
+class TrainConfig(Config):
 	Name = "object" # Give the configuration a recognizable name (don't change it)
+
 	IMAGES_PER_GPU = 1
-	NUM_CLASSES = 1 + 2 # Background + number of custom classes 
+
+	NUM_CLASSES = 1 + 2 # Background + number of custom classes
+
 	STEPS_PER_EPOCH = 10
+
+	# VALIDATION_STEPS = 50 # optional
+
 	DETECTION_MIN_CONFIDENCE = 0.9
+
+class InferenceConfig(Config):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    
+    GPU_COUNT = 1
+    
+    IMAGES_PER_GPU = 1
